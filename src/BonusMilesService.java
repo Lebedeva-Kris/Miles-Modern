b290d2b8f1ae354759059a1358abf5b27c817e65
Kristina <kris2015.lebedeva@yandex.ru>
2020-06-13 00:55:30 +0300

    Java code updated

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,7 +1,6 @@
 public class BonusMilesService {
     public int calculate(int cost) {
-        int amount = 42_000;
-        int miles = amount / 20;
+        int miles = cost / 20;
         return miles;
     }
 }
