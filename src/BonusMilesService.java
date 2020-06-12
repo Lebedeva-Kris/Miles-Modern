d7f1c4c745a691260682e27076b729df005258f2
Kristina <kris2015.lebedeva@yandex.ru>
2020-06-12 19:53:37 +0300

    BobusMilesServices added

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,4 +1,7 @@
-package PACKAGE_NAME;
-
 public class BonusMilesService {
+    public int calculate(int cost) {
+        int amount = 42_000;
+        int miles = amount / 20;
+        return miles;
+    }
 }
